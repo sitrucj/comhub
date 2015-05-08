4af15aa07607bc370f455cc0b9377783983a396c
sitrucj <jccollard3@gmail.com>
2015-05-07 20:08:07 -0400

    message on fail

--- index.php
+++ index.php
@@ -10,12 +10,12 @@
 	require_once __DIR__.'/login.php';
 
 	$conn = new mysqli($hn, $un, $pw, $db);
-	if ($conn->connect_error) die($conn->connect_error);
+	if ($conn->connect_error) mysql_fatal_error($conn->connect_error);
 	
 	$query = "SELECT * FROM complaint";
 	$result = $conn->query($query);
 
-	if (!$result) die($conn->error);
+	if (!$result) mysql_fatal_error($conn->error);
 
 	$rows = $result->num_rows;
 
@@ -28,6 +28,18 @@
 
 	$result->close();
 	$conn->close();
+
+function mysql_fatal_error($msg)
+{
+	$msg2 = mysql_error();
+	echo <<< _END
+The task was not completed. The error message was:
+<p> $msg: $msg2</p>
+Please click the back button and try again._END
+_END;
+die();
+}
+
 ?>
 </p>
 </body>
