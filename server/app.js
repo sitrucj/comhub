0a3cdf182c9cd4a965491c0f05202fb75537bec3
sitrucj <jccollard3@gmail.com>
2015-06-29 10:34:12 -0400

    closer to linking server with angular updated view engin to use html, need to have a error html file in the view folder

--- server/app.js
+++ server/app.js
@@ -20,8 +20,9 @@ mongoose.connect("mongodb://localhost/comdb");
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, '../.tmp'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../.tmp/serve'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
@@ -31,7 +32,7 @@ app.use(session({
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../dist')));
+app.use(express.static(path.join(__dirname, '../app')));
 
 // Passport
 app.use(passport.initialize());
@@ -41,7 +42,7 @@ app.use('/', index);
 app.use('/auth', auth);
 app.use('/api', api);
 
-// catch 404 and forward to error handler
+// catch 404 and forward to handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
